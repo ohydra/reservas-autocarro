aa1dd434cde4ec927b1254f99684fa8751e4c37c
Miguel Andrade <Miguel.Andrade.T0130440@edu.atec.pt>
2025-10-06 20:19:53 +0100

    adicionado menu

--- reservas-autocarro/Program.cs
+++ reservas-autocarro/Program.cs
@@ -12,9 +12,88 @@ namespace reservas_autocarro
         {
 
 
-            //dasddad
-            //dfdasd
-            /////ASDASDASD
+
+
+
+
+
+
+
+
+
+            string opcao;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("=== Sistema de Reservas de Autocarro ===");
+                Console.WriteLine("1) Listar viagens");
+                Console.WriteLine("2) Ver mapa de lugares");
+                Console.WriteLine("3) Criar reserva");
+                Console.WriteLine("4) Cancelar reserva");
+                Console.WriteLine("5) Procurar reservas por passageiro");
+                Console.WriteLine("0) Sair");
+                Console.WriteLine("----------------------------------------");
+                Console.Write("Escolha uma opção: ");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("\n[Opção: Listar viagens]");
+                        // Chamar método ListarViagens();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("\n[Opção: Ver mapa de lugares]");
+                        // Chamar método VerMapa();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("\n[Opção: Criar reserva]");
+                        // Chamar método CriarReserva();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("\n[Opção: Cancelar reserva]");
+                        // Chamar método CancelarReserva();
+                        break;
+
+                    case "5":
+                        Console.WriteLine("\n[Opção: Procurar reservas por passageiro]");
+                        // Chamar método ProcurarReservas();
+                        break;
+
+                    case "0":
+                        Console.WriteLine("\nA sair do sistema... até breve!");
+                        break;
+
+                    default:
+                        Console.WriteLine("\nOpção inválida! Tente novamente.");
+                        break;
+                }
+
+                if (opcao != "0")
+                {
+                    Console.WriteLine("\nPrima qualquer tecla para continuar...");
+                    Console.ReadKey();
+                }
+
+            } while (opcao != "0");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
